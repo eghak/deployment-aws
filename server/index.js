db50032d67ab2594db7ca08969a74b272287080c
Egha Kusuma <eghakusuma1@gmail.com>
2023-02-07 16:56:52 -0700

    update dirname filepath

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(express.static(`${__dirname}../public`))
+app.use(express.static(`${__dirname}/../public`))
 
 
 app.listen(4000, () => console.log('on 4000'))
