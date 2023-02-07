a40c20a12fc84c87ef13ae78208ecc45b053e47c
Egha Kusuma <eghakusuma1@gmail.com>
2023-02-07 11:25:59 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.static(`${__dirname}/public`))
+
+
+app.listen(4000, () => console.log('on 4000'))
